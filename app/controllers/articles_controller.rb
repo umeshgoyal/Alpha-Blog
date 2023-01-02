af29431069c6c4d51957071c05c94f2b9c872705
Umesh Goyal <30938385+umeshgoyal@users.noreply.github.com>
2023-01-03 00:22:47 +0530

    created alpha-blog with CRUD operaion (2-jan-2023)

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,60 @@
 class ArticlesController < ApplicationController
-
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
   def show
+    # @article = Article.find(params[:id])
+  end
+
+  def index
+    @articles = Article.all
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def edit
     @article = Article.find(params[:id])
+  end
+
+  def create
+    # render plain: params[:article]
+
+    # @article = Article.new(params.require(:article).permit(:title,:description))
+    @article = Article.new(article_params)
+    # render plain: @article.inspect
+    if @article.save
+      flash[:notice] = "Article was created successfully"
+      redirect_to @article
+    else
+      render 'new'
+    end
+    # redirect_to article_path(@article) another simple way also
+   
+  end
+
+  def update
+    # @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Article was updated successfully"
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    # @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
+  end
+
+  private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+  
+  def article_params
+    params.require(:article).permit(:title,:description)
   end
 end
